1851d08121afc9489eb2ab3bdd95b425adfe08af
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-15 23:01:30 +0930

    create app factory

---
+++ applications/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask
+
+from applications.misc.constants import BASE_DIR
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=False, static_folder=BASE_DIR + '/static/')
+
+    # app.register_error_handler(400, bad_request)
+    # app.register_error_handler(401, unauthorized)
+    # app.register_error_handler(404, not_found)
+    # app.register_error_handler(405, method_not_allowed)
+    # app.register_error_handler(500, internal_server_error)
+
+    # admin panel configs
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    #
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=False)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
+
+app = create_app()
+
