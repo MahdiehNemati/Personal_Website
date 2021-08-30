145ca164d9ac47ebd0a77021d3513f141e09d01d
mohammad <mohammad_hasani@live.com>
2021-08-30 18:45:48 +0430

    connect to github

--- applications/controllers/public.py
+++ applications/controllers/public.py
@@ -1,8 +1,14 @@
-from flask_classful import FlaskView
+import requests
 from flask import render_template
+from flask_classful import FlaskView
 
 
 class PublicView(FlaskView):
     route_base = "/"
+
     def get(self):
-        return render_template("/public/index.html")
+        github_url = "https://api.github.com/users/MahdiehNemati/repos"
+        github_data = requests.get(github_url)
+        github_data = github_data.json()
+
+        return render_template("/public/index.html", github_data=github_data)
