e1ac9dc824954f395966cc053c05127e13f01758
mohammad <mohammad_hasani@live.com>
2021-09-16 18:10:37 +0430

    add star and fork numbers

--- applications/controllers/github_projects.py
+++ applications/controllers/github_projects.py
@@ -1,4 +1,4 @@
-from docutils.core import publish_parts
+import requests
 from flask import render_template
 from flask_classful import FlaskView, route
 
@@ -8,4 +8,21 @@ class GithubProjectsView(FlaskView):
 
     @route('/<path>')
     def project_detail(self, path):
-        return render_template("/github_projects/index.html", path=path)
+        github_repo_stars = "https://api.github.com/repos/MahdiehNemati/{path}/stargazers".format(path=path)
+        github_stars = requests.get(github_repo_stars)
+        if 200 <= github_stars.status_code < 300:
+            github_stars = github_stars.json()
+            star_numbers = len(github_stars)
+        else:
+            star_numbers = 0
+
+        github_repo_forks = "https://api.github.com/repos/MahdiehNemati/{path}/forks".format(path=path)
+        github_forks = requests.get(github_repo_forks)
+        if 200 <= github_forks.status_code < 300:
+            github_forks = github_forks.json()
+            fork_numbers = len(github_forks)
+        else:
+            fork_numbers = 0
+
+        return render_template("/github_projects/index.html", path=path, star_numbers=star_numbers,
+                               fork_numbers=fork_numbers)
